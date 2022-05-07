d0a4289b954efd47ac7030a09eed532d97ccee43
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-05-06 22:28:28 -0300

    Update VetorOrdemCrescente.java

--- Exercicios/VetorOrdemCrescente.java
+++ Exercicios/VetorOrdemCrescente.java
@@ -9,13 +9,13 @@ public class VetorOrdemCrescente {
         for (int i = 0; i < ordemCrescente.length; i++){
             System.out.print("Digite um numero: "); 
 	        ordemCrescente[i] = (in.nextDouble());
-	    }
+	}
            
         Arrays.sort(ordemCrescente);
            
         System.out.print("Ordem crescente:    "); 
        	for (int j = 0; j < ordemCrescente.length; j++) {
        		System.out.print(ordemCrescente[j]+ " | ");       		
-		}
+	}
     }
 }
