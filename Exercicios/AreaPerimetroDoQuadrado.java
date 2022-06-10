f69213c47d81e357559e9593ed51b988c3b3c862
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 15:44:32 -0300

    Update AreaPerimetroDoQuadrado.java

--- Exercicios/AreaPerimetroDoQuadrado.java
+++ Exercicios/AreaPerimetroDoQuadrado.java
@@ -2,18 +2,12 @@ import java.util.Scanner;
 
 public class AreaPerimetroDoQuadrado {
 	public static void main(String[] args) {
-
 		Scanner in = new Scanner(System.in);
 
-		double lado,area,perimetro;
-
 		System.out.println("Digite o valor do lado ");
-        	lado =in.nextDouble();
-
-        	area=lado*lado;
-		perimetro=lado*4;
-
-       		System.out.println("A área do quadrado é: " + area);
-		System.out.println("O perimetro do quadrado é: " + perimetro);
+        	double lado =in.nextDouble();
+		
+       		System.out.println("A área do quadrado é: "+(lado*lado));
+		System.out.println("O perimetro do quadrado é: "+(lado*4));
 	}
 }
