c8857af95d6447018c98ddff397709f30bf2d195
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 16:03:56 -0300

    Update ForSequenciaFibonacci.java

--- Exercicios/ForSequenciaFibonacci.java
+++ Exercicios/ForSequenciaFibonacci.java
@@ -1,6 +1,5 @@
 public class ForSequenciaFibonacci {
     public static void main(String args[]) {
-
         for(int cont=0,posterior=1;cont<=100;posterior+=cont,cont=posterior-cont){
             System.out.print(cont+"|");
         }
