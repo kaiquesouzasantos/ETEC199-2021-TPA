cba546404d34c0f840b01e16bcbfd22f6485b38d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 15:52:33 -0300

    Update IfIntroducaoParImpar.java

--- Exercicios/IfIntroducaoParImpar.java
+++ Exercicios/IfIntroducaoParImpar.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 
 public class IfIntroducaoParImpar {
     public static void main(String args[]) {
-
         Scanner in= new Scanner (System.in);
 
-        int n;
-
         System.out.println ("Digite um número:");
-        n =in.nextInt();
+        int n =in.nextInt();
 
         if (n%2==0) {
             System.out.println ("O número digitado é classificado como par");
