d29d2129641529e16a3ad790a835ef87f93a472f
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 15:57:14 -0300

    Update IfIntroducaoParImpar.java

--- Exercicios/IfIntroducaoParImpar.java
+++ Exercicios/IfIntroducaoParImpar.java
@@ -4,7 +4,7 @@ public class IfIntroducaoParImpar {
     public static void main(String args[]) {
         Scanner in= new Scanner (System.in);
 
-        System.out.println ("Digite um número:");
+        System.out.print("Digite um número:");
         int n =in.nextInt();
 
         if (n%2==0) {
