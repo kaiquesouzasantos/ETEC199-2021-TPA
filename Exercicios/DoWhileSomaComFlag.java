8fca95e294a388364465024d738dc9884a18656b
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 16:03:39 -0300

    Update DoWhileSomaComFlag.java

--- Exercicios/DoWhileSomaComFlag.java
+++ Exercicios/DoWhileSomaComFlag.java
@@ -2,20 +2,19 @@ import java.util.Scanner;
 
 public class DoWhileSomaComFlag {
     public static void main(String args[]) {
-
         Scanner in= new Scanner (System.in);
 
         int num, soma=0;
         String sequencia;
 
         do{
-            System.out.println("Digite um número inteiro: ");
+            System.out.print("Digite um número inteiro: ");
             num = in.nextInt();
 
-            System.out.println("Quer continuar? [S/N]");
+            System.out.print("Quer continuar? [S/N]");
             sequencia = in.next().toUpperCase();
             soma+=num;
-        }while (sequencia.equals("S"));
+        }while(sequencia.equals("S"));
 
         System.out.println("A soma dos valores é "+soma);
     }
