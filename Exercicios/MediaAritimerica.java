b82fd4dd43493272d7492880781be7887f5ff46e
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-10 15:40:48 -0300

    Update MediaAritimerica.java

--- Exercicios/MediaAritimerica.java
+++ Exercicios/MediaAritimerica.java
@@ -1,24 +1,23 @@
 import java.util.Scanner;
 
 public class MediaAritimerica {
-    public static void main(String args[]) {
-        
+    public static void main(String args[]) { 
         Scanner in= new Scanner (System.in);
-        
-        // nota (n), media aritimetica (m)
-        double m,n1,n2,n3,n4;
+       
+        double notas = 0;
         
         System.out.println ("Digite a 1° nota:");
-        n1 =in.nextDouble();
-        System.out.println ("Digite a 2° nota:");
-        n2 =in.nextDouble();
-        System.out.println ("Digite a 3° nota:");
-        n3 =in.nextDouble();
-        System.out.println ("Digite a 4° nota:");
-        n4 =in.nextDouble();
-
-        m =(n1 +n2 + n3+ n4)/4;
+        notas += in.nextDouble();
         
-        System.out.println ("A média aritimética é "+ m);
+        System.out.println ("Digite a 2° nota:");
+        notas += in.nextDouble();
+        
+        System.out.println ("Digite a 3° nota:");
+        notas += in.nextDouble();
+        
+        System.out.println ("Digite a 4° nota:");
+        notas += in.nextDouble();
+        
+        System.out.println ("A média aritimética é "+(nota/4));
     }
 }
