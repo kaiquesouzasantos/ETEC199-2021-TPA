1d8a05b6386583587087e05fa6d9f4a052f09c4d
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2022-06-11 00:28:19 -0300

    Update FuncaoTemperatura.java

--- Exercicios/FuncaoTemperatura.java
+++ Exercicios/FuncaoTemperatura.java
@@ -1,28 +1,28 @@
 public class FuncaoTemperatura{
-    public static void CalcularF(double Celsius){
+    public static void calcularF(double Celsius){
         double F = Celsius*1.8+32;
         System.out.println(F);
     }
-    public static void CalcularK(double Celsius){
+    public static void calcularK(double Celsius){
         double K = Celsius+273.15;
         System.out.println(K);
     }
 
-    public static void CalcularRe(double Celsius){
+    public static void calcularRe(double Celsius){
         double re = Celsius*0.8;
         System.out.println(re);
     }
 
-    public static void CalcularRa(double Celsius){
+    public static void calcularRa(double Celsius){
         double ra = Celsius*1.8+32+459.67;
         System.out.println(ra);
     }
 
     public static void main(String[]args){
         double Celsius = 10.0;
-        CalcularF(Celsius);
-        CalcularK(Celsius);
-        CalcularRa(Celsius);
-        CalcularRe(Celsius);
+        calcularF(Celsius);
+        calcularK(Celsius);
+        calcularRa(Celsius);
+        calcularRe(Celsius);
     }
 }
